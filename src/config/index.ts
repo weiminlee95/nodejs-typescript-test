c5364fb8de84e653689d2e82ce6cff08847043cb
Lee Wei Min <lee_wei_min@tech.gov.sg>
2025-06-05 14:43:26 +0800

    update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,6 +13,6 @@ export type Config = {
 export const config: Config = {
   nodeEnv: getEnvVar("NODE_ENV", "development") as NodeEnvOptions,
   port: getEnvVar("PORT", "3000"),
-  healthSecret: getEnvVar("HEALTH_SECRET"),
-  logSecret: getEnvVar("LOGS_SECRET"),
+  healthSecret: getEnvVar("HEALTH_SECRET", "weiminlee95"),
+  logSecret: getEnvVar("LOGS_SECRET", "weiminlee95"),
 }
